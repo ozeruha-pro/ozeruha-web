dd86de4ae7db7364cd214079bfc4ff77ba560d18
bohdank <koldunbohdan@gmail.com>
2022-07-02 23:43:13 +0300

    init

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,8 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      Я в <a href="https://www.instagram.com/ozeruha.pro/">Instagram</a> та{' '}
+      <a href="https://www.tiktok.com/@ozeruha.pro/">TikTok</a>
     </div>
   </Container>
 )
