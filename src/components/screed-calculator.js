67d0db9f25193f7185a9690504bca179629ace64
bohdank <koldunbohdan@gmail.com>
2022-07-16 12:48:17 +0300

    the first calculator wip

--- src/components/screed-calculator.js
+++ src/components/screed-calculator.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { InputNumber, Space, Collapse, Table, Divider, Button, Col, Row, Statistic } from 'antd'
+import { InputNumber, Space, Table, Button, Col, Row, Statistic, Card } from 'antd'
 
-const { Panel } = Collapse
-
-const roundTwoDecimal = num => Math.round(num*100)/100
+const roundTwoDecimal = num => Math.round(num * 100) / 100
 
 const materialColumns = [
   {
     title: 'Назва',
     dataIndex: 'name',
     key: 'name',
-    render: (text) => <i>{text}</i>,
+    render: (text) => <b>{text}</b>,
   },
   {
     title: 'Ціна',
@@ -28,7 +26,7 @@ const materialCalculationColumns = [
     title: 'Назва',
     dataIndex: 'name',
     key: 'name',
-    render: (text) => <a>{text}</a>,
+    render: (text) => <b>{text}</b>,
   },
   {
     title: 'Кількість',
@@ -73,7 +71,7 @@ const MATERIALS_PRICE = {
     price: 350,
     unit: 'грн/кан',
   },
-DAMPER_TAPE: {
+  DAMPER_TAPE: {
     name: 'Демпферна лєнта',
     price: 460,
     unit: 'грн/рул',
@@ -92,45 +90,51 @@ const ScreedCalculator = () => {
 
   const [materialPriceTableData, setMaterialTableData] = useState([])
 
+  const [activeTabKey1, setActiveTabKey1] = useState('tab1')
+
+  const onTab1Change = (key) => {
+    setActiveTabKey1(key)
+  }
+
   useEffect(() => {
     const workPrice = workPriceM2
     const materialPriceData = [
       {
         key: '1',
         name: MATERIALS_PRICE.SAND.name,
-        amount: roundTwoDecimal((square * height/100)*1.6),
-        price: roundTwoDecimal((square * height/100)*1.6*MATERIALS_PRICE.SAND.price),
+        amount: roundTwoDecimal((square * height / 100) * 1.6),
+        price: roundTwoDecimal((square * height / 100) * 1.6 * MATERIALS_PRICE.SAND.price),
         unit: 'т',
         currency: 'грн',
       },
       {
         key: '2',
         name: MATERIALS_PRICE.CEMENT.name,
-        amount: roundTwoDecimal((square * height*0.01/0.17)*1.05),
-        price: roundTwoDecimal((square * height*0.01/0.17)*1.05*MATERIALS_PRICE.CEMENT.price),
+        amount: roundTwoDecimal((square * height * 0.01 / 0.17) * 1.05),
+        price: roundTwoDecimal((square * height * 0.01 / 0.17) * 1.05 * MATERIALS_PRICE.CEMENT.price),
         unit: 'т',
         currency: 'грн',
       },
       {
         key: '3',
         name: MATERIALS_PRICE.FIBER.name,
-        amount:roundTwoDecimal(square/500),
-        price: roundTwoDecimal((square/500)*MATERIALS_PRICE.FIBER.price),
+        amount: roundTwoDecimal(square / 500),
+        price: roundTwoDecimal((square / 500) * MATERIALS_PRICE.FIBER.price),
         unit: 'т',
         currency: 'грн',
       },
       {
         key: '4',
         name: MATERIALS_PRICE.SOFTENER.name,
-        amount: roundTwoDecimal(square/300),
-        price: roundTwoDecimal((square/300)*MATERIALS_PRICE.SOFTENER.price),
+        amount: roundTwoDecimal(square / 300),
+        price: roundTwoDecimal((square / 300) * MATERIALS_PRICE.SOFTENER.price),
         unit: 'т',
         currency: 'грн',
       },
       {
         key: '5',
         name: MATERIALS_PRICE.DAMPER_TAPE.name,
-        amount: roundTwoDecimal(square/300),
+        amount: roundTwoDecimal(square / 300),
         price: roundTwoDecimal(MATERIALS_PRICE.DAMPER_TAPE.price),// check
         unit: 'т',
         currency: 'грн',
@@ -138,32 +142,57 @@ const ScreedCalculator = () => {
       {
         key: '6',
         name: 'Доставка',
-        amount: roundTwoDecimal((square/100)),
-        price: roundTwoDecimal((square/100)*700),
+        amount: roundTwoDecimal((square / 100)),
+        price: roundTwoDecimal((square / 100) * 700),
         unit: 'шт',
         currency: 'грн',
       },
     ]
-    const materialPriceSum = materialPriceData.reduce((sum, item)=> sum + item.price, 0)
+    const materialPriceSum = materialPriceData.reduce((sum, item) => sum + item.price, 0)
 
     setWorkPriceM2(workPrice)
-    setWorkPriceSum(square*workPrice)
+    setWorkPriceSum(square * workPrice)
     setMaterialTableData(materialPriceData)
     setMaterialPriceSum(roundTwoDecimal(materialPriceSum))
-    setMaterialPriceM2(roundTwoDecimal(materialPriceSum/square))
+    setMaterialPriceM2(roundTwoDecimal(materialPriceSum / square))
   }, [square, height])
 
-  return (
-    <div>
-      <Divider orientation='left'>Калькулятор</Divider>
-      <Space align="center" wrap>
-        <InputNumber size="large" style={{ maxWidth: '300px' }} stringMode addonBefore='Площа' addonAfter='м2' value={square} onChange={setSquare} />
-        <InputNumber size="large" style={{ maxWidth: '300px' }} addonBefore='Товщина' addonAfter='см' value={height} onChange={setHeight} max={15} min={6} />
+  const tabList = [
+    {
+      key: 'tab1',
+      tab: 'Ціна',
+    },
+    {
+      key: 'tab2',
+      tab: 'Матеріали',
+    },
+  ]
+
+  const contentList = {
+    tab1: <div>
+      <Space align='center' wrap>
+        <InputNumber
+          size='large'
+          style={{ maxWidth: '300px' }}
+          stringMode
+          addonBefore='Площа'
+          addonAfter='м2'
+          value={square} onChange={setSquare}
+        />
+        <InputNumber
+          size='large'
+          style={{ maxWidth: '300px' }}
+          addonBefore='Товщина'
+          addonAfter='см'
+          value={height}
+          onChange={setHeight} max={15} min={6}
+        />
       </Space>
-      <br /> <br />
+      <br />
+      <br />
       <Row gutter={16}>
         <Col span={12}>
-          <Statistic title='Робота' value={workPriceSum}  suffix='грн' />
+          <Statistic title='Робота' value={workPriceSum} suffix='грн'  />
         </Col>
         <Col span={12}>
           <Statistic title='Робота за м2' value={workPriceM2} suffix='грн/м2' />
@@ -175,25 +204,40 @@ const ScreedCalculator = () => {
           <Statistic title='Матеріали за м2' value={materialPriceM2} suffix='грн/м2' />
         </Col>
         <Col span={12}>
-          <Statistic title='Ціна за роботу та матеріали за м2' value={materialPriceM2 + workPriceM2} suffix='грн/м2'/>
+          <Statistic title='Ціна за роботу та матеріали за м2' value={materialPriceM2 + workPriceM2} suffix='грн/м2' />
         </Col>
       </Row>
-      <br /> <br />
-      <Collapse>
-        <Panel header='Детальний розрахунок матеріалів'>
-          <Table columns={materialColumns} dataSource={Object.values(MATERIALS_PRICE)} pagination={false} showHeader={false} bordered
-                 title={() => 'Ціна на матеріали'} />
-          <br/><br/>
-          <Table columns={materialCalculationColumns} dataSource={materialPriceTableData} pagination={false}
-                 showHeader={false} bordered title={() => 'Матеріали необхідні для вашої стяжки'} />
-        </Panel>
-      </Collapse>
+    </div>,
+    tab2: <div>
+      <Table columns={materialCalculationColumns} dataSource={materialPriceTableData} pagination={false}
+             showHeader={false} bordered title={() => `Матеріали необхідні для вашої стяжки ${square} м2 товщиною ${height} см`} />
+      <br /><br />
+      <Table columns={materialColumns} dataSource={Object.values(MATERIALS_PRICE)} pagination={false} showHeader={false}
+             bordered
+             title={() => 'Ціна на матеріали'} />
+    </div>,
+  }
+
+  return (
+    <>
+      <Card
+        style={{ width: '100%' }}
+        title='Калькулятор'
+        extra={<a href='#'>Більше?</a>}
+        tabList={tabList}
+        activeTabKey={activeTabKey1}
+        onTabChange={key => {
+          onTab1Change(key)
+        }}
+      >
+        {contentList[activeTabKey1]}
+      </Card>
       <br /> <br />
       <Button type='primary' key='console'>
         Відправити розрахунки прорабу
       </Button>
       <br /> <br />
-    </div>
+    </>
   )
 }
 
