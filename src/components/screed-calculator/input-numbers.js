d29f1f6c11bf4058728ac4b642c0ed42baab7ee1
bohdank <koldunbohdan@gmail.com>
2022-07-23 21:02:50 +0300

    refactor wip

--- src/components/screed-calculator/input-numbers.js
+++ src/components/screed-calculator/input-numbers.js
@@ -5,6 +5,7 @@ export const InputNumbers = ({ square, setSquare, height, setHeight }) => {
   return (
     <Space align="center" wrap>
       <InputNumber
+        type="number"
         size="large"
         style={{ maxWidth: '300px' }}
         addonBefore="Площа&nbsp;&nbsp;&nbsp;"
@@ -13,14 +14,15 @@ export const InputNumbers = ({ square, setSquare, height, setHeight }) => {
         onChange={setSquare}
       />
       <InputNumber
+        type="number"
         size="large"
         style={{ maxWidth: '300px' }}
         addonBefore="Товщина"
         addonAfter="см"
         value={height}
         onChange={setHeight}
-        min={6}
-        max={15}
+        min={4}
+        max={25}
       />
     </Space>
   )
